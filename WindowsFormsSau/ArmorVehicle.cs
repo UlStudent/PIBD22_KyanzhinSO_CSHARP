056f8687acf5a35f2d9923e71f34919708a7e634
UlStudent <ksergeiii@mail.ru>
2020-11-30 14:58:47 +0300

    final

--- WindowsFormsSau/ArmorVehicle.cs
+++ WindowsFormsSau/ArmorVehicle.cs
@@ -7,74 +7,74 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsSau
 {
-	public class ArmorVehicle : Vehicle
-	{
-		protected readonly int sauWidth = 70;
-		protected readonly int sauHeight = 50;
+    public class ArmorVehicle : Vehicle
+    {
+        protected readonly int sauWidth = 70;
+        protected readonly int sauHeight = 50;
 
-		public ArmorVehicle(int maxSpeed, float weight, Color mainColor)
-		{
-			MaxSpeed = maxSpeed;
-			Weight = weight;
-			MainColor = mainColor;
-		}
+        public ArmorVehicle(int maxSpeed, float weight, Color mainColor)
+        {
+            MaxSpeed = maxSpeed;
+            Weight = weight;
+            MainColor = mainColor;
+        }
 
-		protected ArmorVehicle(int maxSpeed, float weight, Color mainColor, int sauWidth, int sauHeight)
-		{
-			MaxSpeed = maxSpeed;
-			Weight = weight;
-			MainColor = mainColor;
-			this.sauWidth = sauWidth;
-			this.sauHeight = sauHeight;
-		}
+        protected ArmorVehicle(int maxSpeed, float weight, Color mainColor, int sauWidth, int sauHeight)
+        {
+            MaxSpeed = maxSpeed;
+            Weight = weight;
+            MainColor = mainColor;
+            this.sauWidth = sauWidth;
+            this.sauHeight = sauHeight;
+        }
 
-		public override void MoveTransport(Direction direction)
-		{
-			float step = MaxSpeed * 100 / Weight;
-			switch (direction)
-			{
-				case Direction.Right:
-					if (_startPosX + step < _pictureWidth - sauWidth)
-					{
-						_startPosX += step;
-					}
-					break;
-				case Direction.Left:
-					if (_startPosX - step > 0)
-					{
-						_startPosX -= step;
-					}
-					break;
-				case Direction.Up:
-					if (_startPosY - step > 0)
-					{
-						_startPosY -= step;
-					}
-					break;
-				case Direction.Down:
-					if (_startPosY + step < _pictureHeight - sauHeight)
-					{
-						_startPosY += step;
-					}
-					break;
-			}
-		}
+        public override void MoveTransport(Direction direction)
+        {
+            float step = MaxSpeed * 100 / Weight;
+            switch (direction)
+            {
+                case Direction.Right:
+                    if (_startPosX + step < _pictureWidth - sauWidth)
+                    {
+                        _startPosX += step;
+                    }
+                    break;
+                case Direction.Left:
+                    if (_startPosX - step > 0)
+                    {
+                        _startPosX -= step;
+                    }
+                    break;
+                case Direction.Up:
+                    if (_startPosY - step > 0)
+                    {
+                        _startPosY -= step;
+                    }
+                    break;
+                case Direction.Down:
+                    if (_startPosY + step < _pictureHeight - sauHeight)
+                    {
+                        _startPosY += step;
+                    }
+                    break;
+            }
+        }
 
-		public override void DrawTransport(Graphics g)
-		{
-			Pen pen = new Pen(Color.Black);
-			Brush brBlack = new SolidBrush(Color.Black);
-			//gusenica
-			g.FillEllipse(brBlack, _startPosX, _startPosY + sauHeight - 15, 20, 15);
-			g.FillEllipse(brBlack, _startPosX + 19, _startPosY + sauHeight - 15, 20, 15);
-			g.FillEllipse(brBlack, _startPosX + 38, _startPosY + sauHeight - 15, 20, 15);
-			g.DrawRectangle(pen, _startPosX + 9, _startPosY + sauHeight - 15, 40, 15);
+        public override void DrawTransport(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black);
+            Brush brBlack = new SolidBrush(Color.Black);
+            //gusenica
+            g.FillEllipse(brBlack, _startPosX, _startPosY + sauHeight - 15, 20, 15);
+            g.FillEllipse(brBlack, _startPosX + 19, _startPosY + sauHeight - 15, 20, 15);
+            g.FillEllipse(brBlack, _startPosX + 38, _startPosY + sauHeight - 15, 20, 15);
+            g.DrawRectangle(pen, _startPosX + 9, _startPosY + sauHeight - 15, 40, 15);
 
-			Brush brMainColor = new SolidBrush(MainColor);
-			//bashnya
-			g.FillRectangle(brMainColor, _startPosX + 10, _startPosY + sauHeight - 30, 30, 15);
-			//dulo
-			g.FillRectangle(brBlack, _startPosX + 40, _startPosY + sauHeight - 24, 30, 2);
-		}
-	}
+            Brush brMainColor = new SolidBrush(MainColor);
+            //bashnya
+            g.FillRectangle(brMainColor, _startPosX + 10, _startPosY + sauHeight - 30, 30, 15);
+            //dulo
+            g.FillRectangle(brBlack, _startPosX + 40, _startPosY + sauHeight - 24, 30, 2);
+        }
+    }
 }
